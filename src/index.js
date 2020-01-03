a03c8af0c628227c126d2e6829ccf4a9d32f7fef
shmohamud <shmohamud@email.wm.edu>
2020-01-03 13:56:38 -0500

    index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
 import App from "./App/index.jsx";
 import * as serviceWorker from "./serviceWorker";
-import { node } from "prop-types";
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
-
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache({ addTypename: false }),
