484f102372f1070a30bc514fcda797490c930eba
shmohamud <shmohamud@email.wm.edu>
2021-01-15 19:34:05 -0500

    uri edit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const httpLink = new HttpLink({ uri: "https://block-explorer-server.herokuapp.com" });
+const httpLink = new HttpLink({ uri: "https://block-explorer-server.herokuapp.com/graphql" });
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache({ addTypename: false }),
