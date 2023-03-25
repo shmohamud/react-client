e90826af96bdd933285346dd1a7cf977254b40f2
Sahnun <shmohamud@email.wm.edu>
2023-03-25 13:55:31 -0400

    uri update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const httpLink = new HttpLink({ uri: "https://block-explorer-server.netlify.app" });
+const httpLink = new HttpLink({ uri: "https://block-explorer-server.netlify.app/graphql" });
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache({ addTypename: false }),
